dc22fb9133f409267ddce5975239ac780e97a1c5
Berkay Saray <berkaysarayy@hotmail.com>
2022-04-14 15:17:48 +0300

    odev1 Edited

--- odev1/odev1.cpp
+++ odev1/odev1.cpp
@@ -14,28 +14,27 @@ void compareTriplets(int a[3], int b[3]) {
             bobPoints++;
     }
     c[0] = alicePoints; c[1] = bobPoints;
-    cout << c[0] << " " << c[1];
+    cout << "Alice : " << c[0] << " " << "Bob : " << c[1];
 }
 
 int main()
 {
-    int aliceProblemClarity, aliceOriginality, aliceDifficulty;
-    int bobProblemClarity, bobOriginality, bobDifficulty;
+    
     int a[3], b[3];
     bool isAliceBetween = false;
     bool isBobBetween = false;
     while (isAliceBetween == false) {
-        cout << "Please input Alice's reviewer rates!\n";
-        cin >> aliceProblemClarity >> aliceOriginality >> aliceDifficulty;
-        a[0] = aliceProblemClarity; a[1] = aliceOriginality; a[2] = aliceDifficulty;
+        cout << "Please input Alice's reviewer rates (Your inputs have to in [1,99]!) \n";
+        cin >> a[0] >> a[1] >> a[2] ;
+        
         if (a[0] >= 1 && a[0] <= 100 && a[1] >= 1 && a[1] <= 100 && a[2] >= 1 && a[2] <= 100)
             isAliceBetween = true;
     }
 
     while (isBobBetween == false) {
-        cout << "Please input Bob's reviewer rates!\n";
-        cin >> bobProblemClarity >> bobOriginality >> bobDifficulty;
-        b[0] = bobProblemClarity; b[1] = bobOriginality; b[2] = bobDifficulty;
+        cout << "Please input Bob's reviewer rates (Your inputs have to in [1,99]!) \n";
+        cin >> b[0] >> b[1] >> b[2];
+        
         if (b[0] >= 1 && b[0] <= 100 && b[1] >= 1 && b[1] <= 100 && b[2] >= 1 && b[2] <= 100)
             isBobBetween = true;
     }
